92ea966826e34e22e7f1cd18d55e540bd1df805f
PolliBu <8polli.bu@gmail.com>
2024-01-20 23:00:43 +0200

    add components

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+export const Feedback = () => {
+  return (
+    <div>
+      <p>Good:</p>
+      <p>Neutral:</p>
+      <p>Bad:</p>
+    </div>
+  );
+};
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,9 @@
+export const Options = () => {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </div>
+  );
+};
