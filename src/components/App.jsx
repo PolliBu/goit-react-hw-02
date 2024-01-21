740417dab8d519247ba0f963afd0fabfc3321446
PolliBu <8polli.bu@gmail.com>
2024-01-21 13:22:24 +0200

    Task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,10 +14,24 @@ export function App() {
     setClicks({ ...clicks, [type]: clicks[type] + 1 });
   };
 
+  const total = clicks.good + clicks.neutral + clicks.bad;
+
+  const handleReset = () => {
+    setClicks({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
     <>
       <Description />
-      <Options handleClick={handleClick} />
+      <Options
+        handleClick={handleClick}
+        total={total}
+        handleReset={handleReset}
+      />
       <Feedback clicks={clicks} />
     </>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,9 +1,10 @@
-export const Options = ({ handleClick }) => {
+export const Options = ({ handleClick, total, handleReset }) => {
   return (
     <div>
       <button onClick={() => handleClick('good')}>Good</button>
       <button onClick={() => handleClick('neutral')}>Neutral</button>
       <button onClick={() => handleClick('bad')}>Bad</button>
+      {total > 0 && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 };
